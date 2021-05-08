aedaa936eb7256bb13934facfb6ca66cdba749a2
daniabib <danielsabib@gmail.com>
2021-05-08 15:54:42 -0300

    Section 6-2 END

---
+++ ch6/6-2/trapezoidal.py
@@ -0,0 +1,23 @@
+def trapezoidal(f, a, b, n):
+    h = (b - a) / n
+    f_sum = 0
+    for i in range(1, n):
+        x = a + i*h
+        f_sum = f_sum + f(x)
+    return h*(0.5 * f(a) + f_sum + 0.5 * f(b))
+
+
+def application():
+    from math import exp
+    v = lambda t: 3*(t**2)*exp(t**3)
+    n = int(input('n: '))
+    numerical = trapezoidal(v, 0, 1, n)
+
+    # Comparing with exact result
+    V = lambda t: exp(t**3)  # derived by hand
+    exact = V(1) - V(0)
+    error = abs(exact - numerical)
+    print('n={:d}: {:.16f}, error: {:g}'.format(n, numerical, error))
+
+if __name__ == '__main__':
+    application()
